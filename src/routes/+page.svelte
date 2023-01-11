78a4eb41c76431c08376cdc3196b7017af42a244
= <mat@indowwindows.com>
2023-01-11 11:07:08 -0800

    removed skeleton.dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,14 @@
-<script>
-	import PerspectiveForm from '$lib/Components/PerspectiveForm/PerspectiveForm.svelte';
-	import { writable } from 'svelte/store';
-	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+<script>	
 	import OpenAiForm from '$lib/Components/OpenAiForm/OpenAiForm.svelte';
 	/** @type {import('./$types').ActionData} */
-	const storeTab = writable('a');
 	export let form;
 </script>
 
 <section class="grid grid-flow-row">
-	<TabGroup selected={storeTab} justify={'justify-end'} borderWidth='0'>
-		<Tab value="a">OpenAI</Tab>
-		<Tab value="b">Perspective</Tab>
-	</TabGroup>
-	<div>
-		{#if $storeTab === 'a'}<OpenAiForm {form} />{/if}
-		{#if $storeTab === 'b'}<PerspectiveForm {form} />{/if}
+	<h1 class="font-bold flex justify-end">
+		<a href="https://openai.com/">OpenAI API</a>
+	</h1>
+	<div>		
+		<OpenAiForm {form} />
 	</div>
 </section>
